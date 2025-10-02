94e85516540a8f9f286fbf3fff82e6b12405aa68
Sopon islam <soponalways@gmail.com>
2025-10-02 23:37:13 +0600

    Prisma client Sengeleton

---
+++ lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    // Prevent multiple instances of Prisma Client in development 
+    var prisma : PrismaClient | undefined
+}
+
+export const prisma = global.prisma || new PrismaClient(); 
+if (process.env.NODE_ENV !== "production") {
+    global.prisma = prisma; 
+}
